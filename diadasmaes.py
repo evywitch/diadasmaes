989b83a4c401824a6b2d30b65c671af3c622e956
evywitch <0000122094354xsp@al.educacao.sp.gov.br>
2025-05-05 13:08:15 -0300

    Primeiro Commit

---
+++ diadasmaes.py
@@ -0,0 +1,24 @@
+import time
+
+mensagem=[
+    "────୨ৎ────────୨ৎ────────୨ৎ────────୨ৎ────────୨ৎ────",
+    "             Feliz dia das mães!",
+    "♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡",
+    "Mãe, obrigada por tudo que você fez por mim,",
+    "Você é especial, forte e cheia de amor",
+    "Te amo hoje e sempre",
+    "♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡",
+    "────୨ৎ────────୨ৎ────────୨ৎ────────୨ৎ────────୨ৎ────"
+]
+for linha in mensagem:
+    print(linha)
+    time.sleep(1.5)
+print()
+print("    **     **")
+print("  ****** ******")
+print(" ***************")
+print("  ************")
+print("   **********")
+print("     ******")
+print("      ****")
+print("       **")
